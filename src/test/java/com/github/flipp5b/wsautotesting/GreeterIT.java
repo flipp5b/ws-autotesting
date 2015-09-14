f820f92e43c040897821df2cefbe21c327b59f50
Daniil Filippov <dfilippov@icicall.com>
2015-09-14 23:59:56 +0300

    Add comments

--- src/test/java/com/github/flipp5b/wsautotesting/GreeterIT.java
+++ src/test/java/com/github/flipp5b/wsautotesting/GreeterIT.java
@@ -2,7 +2,6 @@ package com.github.flipp5b.wsautotesting;
 
 import com.eviware.soapui.tools.SoapUITestCaseRunner;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -12,20 +11,37 @@ import org.junit.runner.RunWith;
 
 import java.io.File;
 
+// Указываем Arquillian в качестве JUnit раннера
 @RunWith(Arquillian.class)
 public class GreeterIT {
+    /*
+    Метод, помеченный аннотацией Deployment, формирует архив, который Arquillian будет деплоить
+    в контейнер перед выполнением тестов.
+    По умолчанию для архива создается обертка, позволяющая выполнить тесты в рамках контейнера.
+    В нашем случае в этом необходимости нет, тесты должны запускаться на стороне клиента,
+    поэтому выставляем testable в false.
+    */
     @Deployment(testable = false)
     public static Archive<?> createDeployment() {
+        /*
+        ShrinkWrap позволяет филигранно создавать микродеплойменты, изолируя части приложения,
+        которые мы хотим протестировать, и уменьшая время деплоя. Но, к сожалению, иногда
+        (особенно в больших приложениях с кучей зависимостей) сложно выделить часть для микродеплоймента,
+        в таком случае можно просто взять архив, заботливо собранный Maven-ом.
+        */
         return ShrinkWrap.createFromZipFile(WebArchive.class, new File("target/ws-autotesting.war"));
     }
 
     @Test
-    @RunAsClient
     public void testGreeter() throws Exception {
         SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
+        // Указываем SoapUI путь к файлу проекта
         runner.setProjectFile("src/test/resources/Greeter-soapui-project.xml");
+        // просим его создавать отчет в формате JUnit
         runner.setJUnitReport(true);
+        // и еще печатать отчет в консоль
         runner.setPrintReport(true);
+        // и складывать файлы с отчетами в стандартный для failsafe каталог
         runner.setOutputFolder("target/failsafe-reports");
         runner.run();
     }
